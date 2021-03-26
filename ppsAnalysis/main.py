4f2837b1c87f4d06d961a68335696dfc6b6d5c4e
ryogali <r.likeit@gmail.com>
2021-03-26 18:35:09 -0400

    tested full cover on GALEN

--- ppsAnalysis/main.py
+++ ppsAnalysis/main.py
@@ -89,7 +89,7 @@ def variants_main(arguments):
                 fastq_ID = f.split("-")[-2]
                 ref = arguments.ref + "orf9-1_" + fastq_ID
             elif arguments.mode == "yeast":
-                fastq_ID = f.split(".")[0]
+                fastq_ID = f.split("_")[0]
                 # fastq_ID = r1.split("-")[-2]
                 ref = arguments.ref + "ORF_withpDONR"
             else:
@@ -122,11 +122,11 @@ def variants_main(arguments):
             # extract ID
             fastq_ID = f.split("-")[-2]
         elif arguments.mode == "yeast":
-            fastq_ID = f.split(".")[0]
+            fastq_ID = f.split("_")[0]
         else:
             raise ValueError("Wrong mode")
         sub_output = os.path.join(os.path.abspath(output), fastq_ID)
-        raw_vcf_file = os.path.join(sub_output, f"{fastq_ID}_raw.vcf")
+        raw_vcf_file = os.path.join(sub_output, f"{fastq_ID}_L001_raw.vcf")
         # there should be only one log file in the dir
         log_file = glob.glob(f"{sub_output}/*.log")[0]
         if not os.path.isfile(raw_vcf_file):
@@ -150,8 +150,8 @@ def variants_main(arguments):
             pass
         else:  # yeast
             # first get the genes that are fully covered in the fastq files
-            orfs_df = orfs[orfs["plate"] == "fastq_ID"]
-            print(orfs_df)
+            orfs_df = orfs[orfs["plate"] == fastq_ID]
+          
             analysisYeast = ppsAnalysis.yeast_variant_analysis.yeastAnalysis(raw_vcf_file, fastq_ID, orfs_df)
             full_cover_genes, gene_dict, ref_dict = analysisYeast.get_full_cover()
 
@@ -165,10 +165,27 @@ def variants_main(arguments):
             # save all the genes that are fully covered to the output folder
             fully_covered = pd.DataFrame.from_dict(full_cover_genes, orient='index').reset_index()
             fully_covered.columns = ["gene_ID", "gene_len", "total_rd", "avg_rd"]
+            # split gene ID col 
+            fully_covered["gene_ID"] = fully_covered["gene_ID"].str.replace("gene", "")
+            fully_covered = fully_covered.replace(to_replace ='-index[0-9]+', value = '', regex = True)
+            print(fully_covered["gene_ID"]) 
+            try: 
+                fully_covered[["gene_ID", "db"]] = fully_covered["gene_ID"].str.split("_", expand=True)
+            except:
+                fully_covered["db"] = "smorf"
             fully_covered_file = os.path.join(sub_output, "fully_covered.csv")
             fully_covered.to_csv(fully_covered_file, index=False)
-            print(fully_covered)
-            exit()
+
+            # select from fully_covered based on which db the orf is from
+            if "HIP" in fastq_ID:
+                filtered_db = fully_covered[fully_covered["db"] == "HIP"]
+            else:
+                filtered_db = fully_covered[fully_covered["db"] != "HIP"]
+
+            # merge with target orfs
+            merged_df = pd.merge(orfs_df, filtered_db, how="left", left_on="orf_name", right_on="gene_ID")
+            merged_file = os.path.join(sub_output, "merged_with_targets.csv")
+            merged_df.to_csv(merged_file, index=False)
             genes_found.append([fastq_ID, n_fully_aligned, n_all_found, n_ref])
 
             # merge fully covered gene to the targeted genes in this sample 
@@ -195,14 +212,13 @@ def read_yeast_csv(HIP_target_ORFs, other_target_ORFs):
     HIP_df = pd.read_csv(HIP_target_ORFs)
     other_target_ORFs = pd.read_csv(other_target_ORFs)
 
-    HIP_df = HIP_df[["ORF_NAME_NODASH", "len(seq)", "SYMBOL", "scORFeome_384-format_HIP Plate"]]
+    HIP_df = HIP_df[["ORF_NAME_NODASH", "len(seq)", "SYMBOL", "plate"]]
     HIP_df["db"] = "HIP"
-    HIP_df = HIP_df.rename(columns={"ORF_NAME_NODASH": "orf_name", "scORFeome_384-format_HIP Plate": "plate"})
-    other_ORFs = other_target_ORFs[["orf_name", "src_collection", "Condensed plate"]]
-    other_ORFs = other_ORFs.rename(columns={"src_collection": "db", "Condensed plate": "plate"})
+    HIP_df = HIP_df.rename(columns={"ORF_NAME_NODASH": "orf_name"})
+    other_ORFs = other_target_ORFs[["orf_name", "src_collection", "plate"]]
+    other_ORFs = other_ORFs.rename(columns={"src_collection": "db"})
     other_ORFs['plate'] = 'scORFeome-' + other_ORFs['plate'].astype(str)
-    combined = pd.concat([HIP_df, other_target_ORFs], axis=0, ignore_index=True)
-    print(combined)
+    combined = pd.concat([HIP_df, other_ORFs], axis=0, ignore_index=True)
     return combined
 
 
@@ -227,7 +243,7 @@ def check_args(arguments):
     if arguments.mode == "yeast":
         # load yeast data
         # files contain ORF information in
-        HIP_target_ORFs = "/home/rothlab/rli/02_dev/06_pps_pipeline/target_orfs/HIP_targeted_ORFs"
+        HIP_target_ORFs = "/home/rothlab/rli/02_dev/06_pps_pipeline/target_orfs/HIP_targeted_ORFs.csv"
         other_target_ORFs = "/home/rothlab/rli/02_dev/06_pps_pipeline/target_orfs/other_targeted_ORFs.csv"
         orfs = read_yeast_csv(HIP_target_ORFs, other_target_ORFs)
     elif arguments.mode == "human":
