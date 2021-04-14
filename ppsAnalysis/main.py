ccc5cf39c2d8bfceb2af0dad83940e5694023199
ryogali <r.likeit@gmail.com>
2021-04-14 15:15:50 -0400

    alignment to diff reference tested on GALEN

--- ppsAnalysis/main.py
+++ ppsAnalysis/main.py
@@ -10,13 +10,17 @@ Main script for plasmid pool sequencing analysis
 import glob
 
 import pandas as pd
+import numpy as np
 import os
 import argparse
+import seaborn as sns
 
 import ppsAnalysis.alignment
 import ppsAnalysis.cluster
 import ppsAnalysis.yeast_variant_analysis
 import logging.config
+import matplotlib.pyplot as plt
+from matplotlib_venn import venn2, venn2_circles, venn2_unweighted
 
 
 def variants_main(arguments):
@@ -72,11 +76,12 @@ def variants_main(arguments):
             main_logger.info("Alignment jobs all finished")
 
     # for each sample, parse vcf files
-    all_log = []
+    all_log = {"fastq_ID": [], "reads":[], "map_perc":[]}
     genes_found = []
-    all_genes_summary = pd.DataFrame([], columns=["expected_orf_name", "len(seq)", "SYMBOL", "plate", "db_expected" ,"aligned_orf", "gene_len", "total_rd", "avg_rd", "db_aligned"])
-    all_summary = os.path.join(output, "all_summary.csv")
+    all_genes_summary = pd.DataFrame([], columns=["gene_ID", "gene_len", "total_rd", "avg_rd", "db", "count", "gene_name"])
+    all_summary = os.path.join(output, "all_summary_subsetORF.csv")
     all_genes_summary.to_csv(all_summary, index=False)
+    
     for f in file_list:
         if not f.endswith(".fastq.gz"): continue
         if arguments.mode == "human":
@@ -99,10 +104,11 @@ def variants_main(arguments):
             for line in log_f:
                 if "reads;" in line:
                     n_reads = line.split(" ")[0]
+                    all_log["reads"].append(n_reads)
                 if "alignment rate" in line:
                     perc_aligned = line.split("%")[0]
-            all_log.append([fastq_ID, n_reads, perc_aligned])
-
+                    all_log["map_perc"].append(perc_aligned)
+        all_log["fastq_ID"] += [fastq_ID]*3
         # for each vcf file, get how many genes are fully aligned
         if arguments.mode == "human":
             # extract ID
@@ -112,25 +118,25 @@ def variants_main(arguments):
             orfs_df = orfs[orfs["plate"] == fastq_ID]
             raw_vcf_file = os.path.join(sub_output, f"{fastq_ID}_L001_allORFs_raw.vcf")
             if not os.path.isfile(raw_vcf_file):
+
                 main_logger.warning(f"VCF file does not exist: {raw_vcf_file}")
                 continue
             # analysis of ORFs aligned to all ref + backbone
             fully_covered, stats_list = analysisYeast(raw_vcf_file, fastq_ID, orfs_df)
             fully_covered_file = os.path.join(sub_output, "fully_covered_allORFs.csv")
             fully_covered.to_csv(fully_covered_file, index=False)
-            stats_list = stats_list.append("allORFs")
+            stats_list.append("allORFs")
             genes_found.append(stats_list)
-
+            
             raw_vcf_file = os.path.join(sub_output, f"{fastq_ID}_L001_allwithbackbone_raw.vcf")
-            if not os.path.isfile(raw_vcf_file):
-                main_logger.warning(f"VCF file does not exist: {raw_vcf_file}")
-                continue
-            # analysis of ORFs aligned to all ref
-            fully_covered, stats_list = analysisYeast(raw_vcf_file, fastq_ID, orfs_df)
-            fully_covered_file = os.path.join(sub_output, "fully_covered_allwithbackbone.csv")
-            fully_covered.to_csv(fully_covered_file, index=False)
-            stats_list = stats_list.append("allwithbackbone")
-            genes_found.append(stats_list)
+            print(raw_vcf_file)
+            if os.path.isfile(raw_vcf_file):
+                # analysis of ORFs aligned to all ref
+                fully_covered, stats_list = analysisYeast(raw_vcf_file, fastq_ID, orfs_df)
+                fully_covered_file = os.path.join(sub_output, "fully_covered_allwithbackbone.csv")
+                fully_covered.to_csv(fully_covered_file, index=False)
+                stats_list = stats_list.append("allwithbackbone")
+                genes_found.append(stats_list)
 
             raw_vcf_file = os.path.join(sub_output, f"{fastq_ID}_L001_plateORFs_raw.vcf")
             if not os.path.isfile(raw_vcf_file):
@@ -140,7 +146,8 @@ def variants_main(arguments):
             fully_covered, stats_list = analysisYeast(raw_vcf_file, fastq_ID, orfs_df)
             fully_covered_file = os.path.join(sub_output, "fully_covered_plateORFs.csv")
             fully_covered.to_csv(fully_covered_file, index=False)
-            stats_list = stats_list.append("plateORFs")
+            fully_covered.to_csv(all_summary, index=False, header=False, mode="a")
+            stats_list.append("plateORFs")
             genes_found.append(stats_list)
 
             raw_vcf_file = os.path.join(sub_output, f"{fastq_ID}_L001_subsetORFs_raw.vcf")
@@ -151,7 +158,7 @@ def variants_main(arguments):
             fully_covered, stats_list = analysisYeast(raw_vcf_file, fastq_ID, orfs_df)
             fully_covered_file = os.path.join(sub_output, "fully_covered_subsetORFs.csv")
             fully_covered.to_csv(fully_covered_file, index=False)
-            stats_list = stats_list.append("subsetORFs")
+            stats_list.append("subsetORFs")
             genes_found.append(stats_list)
             
             # # merge with ref to get gene len
@@ -169,16 +176,83 @@ def variants_main(arguments):
             # target_gene_mut_count.to_csv(target_gene_mut_file, index=False)
 
     # process all log
-    all_log_df = pd.DataFrame(all_log, columns=["plate", "total reads", "alignment rate"])
+    all_log = pd.DataFrame(all_log)
+    print(all_log)
     all_log_file = os.path.join(output, "alignment_log.csv")
-    all_log_df.to_csv(all_log_file, index=False)
+    all_log.to_csv(all_log_file, index=False)
 
     # process summary of number of genes found in each sample
+    print(genes_found)
     all_genes_stats = pd.DataFrame(genes_found, columns=["plate", "fully_aligned", "all_genes_found",
                                                          "all_targeted_on_plate", "all_targeted_full",
                                                          "n_genes_with_any_mut", "n_ref", "aligned_to"])
     genes_found_file = os.path.join(output, "genes_stats.csv")
     all_genes_stats.to_csv(genes_found_file, index=False)
+    all_genes_stats["% on plate fully aligned"] = all_genes_stats["all_targeted_full"]/all_genes_stats["all_targeted_on_plate"]
+    print(all_genes_stats)
+    sns.set_theme(style="whitegrid", font_scale=1.5)
+    plt.figure(figsize=(20,14))
+    g = sns.barplot(data = all_genes_stats, x = "plate", y = "% on plate fully aligned", hue="aligned_to")
+    plt.xticks(rotation=25, fontsize=15)
+    plt.yticks(fontsize=15)
+    plt.ylabel("all the fully aligned unique ORFs", fontsize=15)
+    plt.savefig("./test_perc.png")
+    plt.close()
+    print(orfs)
+    print(orfs.columns)
+    # compare genes in all the targeted space (ORFs) vs all fully aligned 
+    all_targeted_unique_db = orfs["ORF_NAME_NODASH"].dropna().unique()
+    print(all_targeted_unique_db)
+    all_found = pd.read_csv(all_summary)
+    print(all_found)
+    print(all_found.columns)
+    print(all_found["db"].unique())
+    all_found["gene_name"] = all_found["gene_name"].replace("-", "")
+    all_found_genes = all_found["gene_name"].dropna().unique()
+    venn2([set(all_targeted_unique_db), set(all_found_genes)], set_labels = ("all ORFs", "all_fully_aligned"))
+    plt.savefig("./testvenn2.png")
+    plt.close()
+    print(np.setdiff1d(all_found_genes, all_targeted_unique_db))
+    print(np.setdiff1d(all_targeted_unique_db, all_found_genes))
+     
+    # HIP subset
+    all_HIP_targeted = orfs[orfs["db"] == "HIP"]["ORF_NAME_NODASH"].dropna().unique() 
+    all_found_hip = all_found[all_found["db"] == "HIP"]
+    all_found_hip["gene_name"] = all_found_hip["gene_name"].replace("-", "")
+    all_found_genes = all_found_hip["gene_name"].dropna().unique()
+    print(all_found_genes)
+    venn2([set(all_HIP_targeted), set(all_found_genes)], set_labels = ("all HIP ORFs", "all_fully_aligned"))
+    plt.savefig("./testvenn2_hip.png")
+    plt.close()
+    print(np.setdiff1d(all_found_genes, all_HIP_targeted))
+    print(np.setdiff1d(all_HIP_targeted, all_found_genes))
+
+    # SGD subset
+    all_HIP_targeted = orfs[orfs["db"] == "SGD"]["ORF_NAME_NODASH"].dropna().unique() 
+    all_found_hip = all_found[all_found["db"] == "SGD"]
+    all_found_hip["gene_name"] = all_found_hip["gene_name"].replace("-", "")
+    all_found_genes = all_found_hip["gene_name"].dropna().unique()
+    print(all_found_genes)
+    venn2([set(all_HIP_targeted), set(all_found_genes)], set_labels = ("all SGD ORFs", "all_fully_aligned"))
+    plt.savefig("./testvenn2_SGD.png")
+    plt.close()
+    print(np.setdiff1d(all_found_genes, all_HIP_targeted))
+    print(np.setdiff1d(all_HIP_targeted, all_found_genes))
+
+
+    # PROTGEN subset
+    all_HIP_targeted = orfs[orfs["db"] == "PROTGEN"]["ORF_NAME_NODASH"].dropna().unique() 
+    all_found_hip = all_found[all_found["db"] == "PROTGEN"]
+    all_found_hip["gene_name"] = all_found_hip["gene_name"].replace("-", "")
+    all_found_genes = all_found_hip["gene_name"].dropna().unique()
+    print(all_found_genes)
+    venn2([set(all_HIP_targeted), set(all_found_genes)], set_labels = ("all PROTGEN ORFs", "all_fully_aligned"))
+    plt.savefig("./testvenn2_PROTGEN.png")
+    plt.close()
+    print(np.setdiff1d(all_found_genes, all_HIP_targeted))
+    print(np.setdiff1d(all_HIP_targeted, all_found_genes))
+
+
 
 
 def read_yeast_csv(HIP_target_ORFs, other_target_ORFs):
@@ -191,10 +265,10 @@ def read_yeast_csv(HIP_target_ORFs, other_target_ORFs):
     HIP_df = pd.read_csv(HIP_target_ORFs)
     other_target_ORFs = pd.read_csv(other_target_ORFs)
 
-    HIP_df = HIP_df[["ORF_id", "len(seq)", "SYMBOL", "plate"]]
+    HIP_df = HIP_df[["ORF_id", "ORF_NAME_NODASH", "len(seq)", "SYMBOL", "plate"]]
     HIP_df["db"] = "HIP"
     HIP_df = HIP_df.rename(columns={"ORF_id": "orf_name"})
-    other_ORFs = other_target_ORFs[["orf_name", "src_collection", "plate"]]
+    other_ORFs = other_target_ORFs[["orf_name", "ORF_NAME_NODASH", "src_collection", "plate"]]
     other_ORFs = other_ORFs.rename(columns={"src_collection": "db"})
     #other_ORFs['plate'] = 'scORFeome-' + other_ORFs['plate'].astype(str)
     combined = pd.concat([HIP_df, other_ORFs], axis=0, ignore_index=True)
@@ -220,10 +294,12 @@ def analysisYeast(raw_vcf_file, fastq_ID, orfs_df):
     fully_covered = pd.DataFrame.from_dict(full_cover_genes, orient='index').reset_index()
     fully_covered.columns = ["gene_ID", "gene_len", "total_rd", "avg_rd"]
     # split gene ID col 
-    #fully_covered["gene_ID"] = fully_covered["gene_ID"].str.replace("gene", "")
-    fully_covered = fully_covered.replace(to_replace ='-index[0-9]+', value = '', regex = True)
-    fully_covered[["gene_ID", "db", "count"]] = fully_covered["gene_ID"].str.split("-", expand=True)
-
+    #fully_covered["gene_ID"] = fully_covered["gene_ID"].str.replace(to_replace="-[A-G]", "A")
+     
+    #fully_covered = fully_covered.replace(to_replace ='-index[0-9]+', value = '', regex = True)
+    fully_covered["db"] = fully_covered["gene_ID"].str.extract(r".*-([A-Z]+)-[1-9]")
+    fully_covered["count"] = fully_covered["gene_ID"].str.extract(r".*-[A-Z]+-([1-9])")
+    fully_covered["gene_name"] = fully_covered["gene_ID"].str.extract(r"(.*)-[A-Z]+-[1-9]")
     # filter vcf based on QUAL and DP
     mut_count_dict = analysis.filter_vcf()
     gene_mut_count = pd.DataFrame.from_dict(mut_count_dict, orient='index').reset_index()
@@ -231,12 +307,12 @@ def analysisYeast(raw_vcf_file, fastq_ID, orfs_df):
     n_mut_genes = len(mut_count_dict)
 
     # merge with target orfs
-    # merged_df = pd.merge(orfs_df, filtered_db, how="left", left_on="orf_name", right_on="gene_ID")
+    merged_df = pd.merge(orfs_df, fully_covered, how="left", left_on="orf_name", right_on="gene_ID")
     # merged_file = os.path.join(sub_output, "merged_with_targets.csv")
     # merged_df.to_csv(merged_file, index=False)
     # merged_df.to_csv(all_summary, mode="a", index=False, header=False)
-    # n_targeted = orfs_df.shape[0]
-    # n_targeted_full = merged_df[~merged_df["gene_ID"].isnull()].shape[0]
+    n_targeted = orfs_df.shape[0]
+    n_targeted_full = merged_df[~merged_df["gene_ID"].isnull()].shape[0]
     stats_list = [fastq_ID, n_fully_aligned, n_all_found, n_targeted, n_targeted_full,n_mut_genes, n_ref]
             
     # # merge with ref to get gene len
